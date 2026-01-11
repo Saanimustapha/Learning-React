b12e072254211401b2b7be15c1e2aaec6f5484d0
Saani Mustapha Deishini <deishini2003@gmail.com>
2026-01-11 22:02:49 +0000

    Modified array state by modifying attribute of an array element.

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -3,21 +3,32 @@ import { useState } from "react"
 export const ToDoList = () => {
 
     const [items, setItems] = useState(
-        [{id:1,text:"First Item"},
-        {id:2,text:"Second Item"},
-        {id:3,text:"Third Item"}
+        [{id:1,text:"First Item",done:false},
+        {id:2,text:"Second Item",done:false},
+        {id:3,text:"Third Item",done:false}
         ]
     )
 
 
     const addItem = (e) => {
-        const newItems = [...items, {id:Date.now, text:"Fourth Item"}];
+        const newItems = [...items, {id:Date.now, text:"Fourth Item",done:false}];
         setItems(newItems)
     }
 
     const removeItem = (id) => {
         const newItems = items.filter((item) => item.id != id)
         setItems(newItems)
+    }
+
+    const toggleDone = (id) => {
+        setItems(items.map((item) => {
+            if(item.id === id){
+                return (
+                    {...item,done: !item.done}
+                )
+            }
+            return item
+        }))
     }
 
 
@@ -32,8 +43,9 @@ export const ToDoList = () => {
                     items.map((item) => {
                         return (
                         <li key={item.id}>
-                            {item.text}
+                            {item.done && item.text}
                             <button onClick={() => removeItem(item.id)}>Remove Item</button>
+                            <button onClick={() => toggleDone(item.id)}>Toggle done</button>
                         </li>
                         )
                     })
